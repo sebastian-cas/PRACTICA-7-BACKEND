46e38c771c8408247bd846f90a13f40f0d7b9674
sebastian castillo <sebastiancastillo305@gmail.com>
2024-06-04 16:25:28 -0500

    agregar metodos http y params

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 console.log('Hola mundo con Node JS')
 
-const express = require('express')
+// forma antigua de llamar libreria
+// const express = require('express')
+
+// forma actual con ECMAScript 6 de llamar librerias
+import express from 'express'
+
+
 const app = express()
 const port = 3000
 
@@ -14,8 +20,66 @@ const port = 3000
 // - el callback recibe 2 parametros:
 // - req: request o la peticion
 // - res: response o la respuesta
-app.get('/', (req, res) => {
-    res.send('Hola mundo!')
+app.get('/api/v1/usuarios', (req, res) => {
+    // const respuesta = {
+    //     mensaje: "hola"
+    // }
+
+    // res.json(respuesta)
+
+    res.json({
+        mensaje: 'lista de usuarios',
+    })
+})
+
+app.get('/api/v1/usuarios/:cedula', (req, res) => {
+
+    console.log(req.params)
+    const cedula = req.params.cedula
+
+    res.json({
+        mensaje: `usuario obtenido con la cedula: ${cedula}`
+    })
+})
+
+// post: crear datos
+app.post('/api/v1/usuarios', (req, res) => {
+
+    res.json({
+        mensaje: 'usuario guardado'
+    })
+})
+
+// put: actualizar todos los
+// datos de un elemento
+app.put('/api/v1/usuarios/:cedula', (req, res) => {
+
+    const cedula = req.params.cedula
+
+    res.json({
+        mensaje: `usuario con cedula ${cedula} actualizado`
+    })
+})
+
+// patch: actualiza algunos campos
+// de nuestro elemetno
+app.patch('/api/v1/usuarios/:cedula', (req, res) => {
+
+    const cedula = req.params.cedula
+
+    res.json({
+        mensaje: `edad del usuario con cedula ${cedula} actualizada`
+    })
+})
+
+// delete: eliminar un elemento
+app.delete('/api/v1/usuarios/:cedula', (req, res) => {
+
+    const cedula = req.params.cedula
+
+    res.json({
+        mensaje: `usuario con cedula ${cedula} eliminado`
+    })
 })
 
 // Le indicamos a nuesta API que empiece a escuchar peticiones
